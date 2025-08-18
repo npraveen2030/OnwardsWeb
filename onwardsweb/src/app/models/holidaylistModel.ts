3f3b2bee366d12bfc8d5c50fc15cdbcbb4571246
npraveen2030 <npraveen2030@gmail.com>
2025-08-18 21:37:13 +0530

    Added Code

---
+++ onwardsweb/src/app/models/holidaylistModel.ts
@@ -0,0 +1,7 @@
+export interface HolidayList {
+  id: number;
+  holidayDate: string;
+  day: string;
+  location: string;
+  holidayName: string;
+}
--- onwardsweb/src/app/services/dashboardservice.service.ts
+++ onwardsweb/src/app/services/dashboardservice.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { UserShiftLogResponse } from '../models/DashBoardResponseModel';
+import { HolidayList } from '../models/holidaylistModel';
+import { TrainingDetails } from '../models/trainingModel';
 
 @Injectable({
   providedIn: 'root',
@@ -31,6 +33,34 @@ export class DashBoardService {
       `${this.apiService}/UserShiftDetails/InsertOrUpdate/${userId}`,
       null, // <-- No request body, so pass `null`
       { headers, withCredentials: true } // <-- Correct place for headers and credentials
+    );
+  }
+
+   getHolidayList(locationId: number | null): Observable<HolidayList> {
+    //TO DO: here 1 is Bangalore
+    locationId = locationId == null ? 1 : locationId;
+    
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+
+    return this.http.get<HolidayList>(
+      `${this.apiService}/HolidayList/GetHolidayByLocationId/${locationId}`,
+      { headers, withCredentials: true }
+    );
+  }
+  
+   getTrainingDetailsByLocationId(locationId: number | null): Observable<TrainingDetails> {
+    //TO DO: here 1 is Bangalore
+    locationId = locationId == null ? 1 : locationId;
+    
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+
+    return this.http.get<TrainingDetails>(
+      `${this.apiService}/Training/GetTrainingDetailsByLocationId/${locationId}`,
+      { headers, withCredentials: true }
     );
   }
 }
